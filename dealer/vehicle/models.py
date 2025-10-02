e7c4cab9bb170ad643f3674ae78825d7456a2632
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2025-10-02 15:20:24 +0100

    dealer: Use placeholder to add extra content

--- dealer/vehicle/models.py
+++ dealer/vehicle/models.py
@@ -1,5 +1,10 @@
+from functools import cached_property
+
 from django.db import models
 from django.shortcuts import reverse
+
+from cms.models.fields import PlaceholderRelationField
+from cms.utils.placeholder import get_placeholder_from_slot
 
 # Create your models here.
 
@@ -14,6 +19,15 @@ class Car(models.Model):
     name = models.CharField(max_length=100)
     color = models.CharField(max_length=100)
     car_type = models.ForeignKey(CarType, on_delete=models.CASCADE)
+
+    placeholders = PlaceholderRelationField()
+
+    @cached_property
+    def content(self):
+        return get_placeholder_from_slot(self.placeholders, "Page Content")
+
+    def get_template(self):
+        return "vehicle/car_detail.html"
 
     def absolute_url(self):
         return reverse("vehicle:cardetail", args=[self.pk])
